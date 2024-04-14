018a4ca0546ae67218fb94c44bb2387cd97a7b63
thandartun-dev <thandar.tun.developer@gmail.com>
2024-04-14 22:39:14 +0630

    complete ado.net CRUD

--- TTDotNetCore.ConsoleApp/AdoDotNetExample.cs
+++ TTDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -49,12 +49,42 @@ public class AdoDotNetExample
         }
     }
 
+    public void Edit(int id)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        string query = "select * from Tbl_Blog where BlogId = @BlogId";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogId", id);
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sqlDataAdapter.Fill(dt);
+
+
+        connection.Close();
+
+        if (dt.Rows.Count == 0)
+        {
+            Console.WriteLine("No data found");
+            return;
+        }
+
+        DataRow dr = dt.Rows[0];
+
+        Console.WriteLine("Blog ID => " + dr["BlogId"]);
+        Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+        Console.WriteLine("Blog Authour => " + dr["BlogAuthor"]);
+        Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+        Console.WriteLine("--------------------------------------");
+
+    }
+
     public void Create(string title, string author, string content)
     {
         SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
 
         connection.Open();
-        Console.WriteLine("Connection Open");
 
         string query = @"INSERT INTO [dbo].[Tbl_Blog]
          (
@@ -74,8 +104,53 @@ public class AdoDotNetExample
         int result = cmd.ExecuteNonQuery();
 
         connection.Close();
-        Console.WriteLine("Connection Close");
+
+        string message = result > 0 ? "Saving Successful" : "Saving Failed";
+        Console.WriteLine(message);
     }
+
+    public void Update(int id, string title, string author, string content)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        string query = @"UPDATE [dbo].[Tbl_Blog]
+                    SET
+                        [BlogTitle] = @BlogTitle,
+                        [BlogAuthor] = @BlogAuthor,
+                        [BlogContent] = @BlogContent
+                    WHERE
+                        BlogId = @BlogId";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogId", id);
+        cmd.Parameters.AddWithValue("@BlogTitle", title);
+        cmd.Parameters.AddWithValue("@BlogAuthor", author);
+        cmd.Parameters.AddWithValue("@BlogContent", content);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+        Console.WriteLine(message);
+    }
+
+    public void Delete(int id)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+
+        string query = @"delete from Tbl_Blog where BlogId = @BlogId";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogId", id);
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+        Console.WriteLine(message);
+    }
+
+
 
 
 }
