12b069c843188ce89f7845c9327eaaf7683d0a18
thandartun-dev <thandar.tun.developer@gmail.com>
2024-04-14 21:45:46 +0630

    ado.net create

---
+++ TTDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace
+TTDotNetCore.ConsoleApp;
+
+public class AdoDotNetExample
+{
+    private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = "localhost", // server Name
+        InitialCatalog = "DotNetTrainingBatch4",  //db name
+        UserID = "sa",
+        Password = "Sasa@123",
+        TrustServerCertificate = true, // Set to true to trust the server's certificate
+    };
+    public void Read()
+    {
+        // SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+        // stringBuilder.DataSource = "localhost"; // server Name
+        // stringBuilder.InitialCatalog = "DotNetTrainingBatch4";  //db name
+        // stringBuilder.UserID = "sa";
+        // stringBuilder.Password = "Sasa@123";
+        // stringBuilder.TrustServerCertificate = true; // Set to true to trust the server's certificate
+        // SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+        connection.Open();
+        Console.WriteLine("Connection Open");
+
+        string query = "SELECT * FROM Tbl_Blog";
+        SqlCommand cmd = new SqlCommand(query, connection); // Associate the command with the connection
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sqlDataAdapter.Fill(dt);
+
+        connection.Close();
+        Console.WriteLine("Connection Close");
+
+        foreach (DataRow dr in dt.Rows)
+        {
+            Console.WriteLine("Blog Id => " + dr["BlogID"]);
+            Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+            Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+            Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+            Console.WriteLine("-------------------------------");
+        }
+    }
+
+    public void Create(string title, string author, string content)
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+        connection.Open();
+        Console.WriteLine("Connection Open");
+
+        string query = @"INSERT INTO [dbo].[Tbl_Blog]
+         (
+            [BlogTitle]
+            ,[BlogAuthor]
+            ,[BlogContent]
+         )
+         VALUES
+         (@BlogTitle
+         ,@BlogAuthor
+         ,@BlogContent)";
+        SqlCommand cmd = new SqlCommand(query, connection); // Associate the command with the connection
+        cmd.Parameters.AddWithValue("@BlogTitle", title);
+        cmd.Parameters.AddWithValue("@BlogAuthor", author); // Correct parameter name
+        cmd.Parameters.AddWithValue("@BlogContent", content); // Correct parameter name
+
+        int result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+        Console.WriteLine("Connection Close");
+    }
+
+
+}
--- TTDotNetCore.ConsoleApp/Program.cs
+++ TTDotNetCore.ConsoleApp/Program.cs
@@ -1,36 +1,43 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using TTDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "localhost"; // server Name
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4";  //db name
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "Sasa@123";
-stringBuilder.TrustServerCertificate = true; // Set to true to trust the server's certificate
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+// SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+// stringBuilder.DataSource = "localhost"; // server Name
+// stringBuilder.InitialCatalog = "DotNetTrainingBatch4";  //db name
+// stringBuilder.UserID = "sa";
+// stringBuilder.Password = "Sasa@123";
+// stringBuilder.TrustServerCertificate = true; // Set to true to trust the server's certificate
+// SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 
-connection.Open();
-Console.WriteLine("Connection Open");
+// connection.Open();
+// Console.WriteLine("Connection Open");
 
-string query = "SELECT * FROM Tbl_Blog";
-SqlCommand cmd = new SqlCommand(query, connection); // Associate the command with the connection
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
+// string query = "SELECT * FROM Tbl_Blog";
+// SqlCommand cmd = new SqlCommand(query, connection); // Associate the command with the connection
+// SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+// DataTable dt = new DataTable();
+// sqlDataAdapter.Fill(dt);
 
-connection.Close();
-Console.WriteLine("Connection Close");
+// connection.Close();
+// Console.WriteLine("Connection Close");
 
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("Blog Id => " + dr["BlogID"]);
-    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
-    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
-    Console.WriteLine("-------------------------------");
-}
+// foreach (DataRow dr in dt.Rows)
+// {
+//     Console.WriteLine("Blog Id => " + dr["BlogID"]);
+//     Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+//     Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+//     Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+//     Console.WriteLine("-------------------------------");
+// }
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+// adoDotNetExample.Read();
+
+adoDotNetExample.Create("title", "author", "content");
+adoDotNetExample.Read();
 
 Console.ReadKey();
